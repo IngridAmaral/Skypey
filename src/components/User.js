286fd7a60b66fe948ead06aff627a8f317f80998
Ingrid Amaral <guid_95@hotmail.com>
2020-03-16 11:22:30 +0100

    user component style

--- src/components/User.js
+++ src/components/User.js
@@ -3,9 +3,14 @@ import '../css/User.css';
 
 class User extends React.Component {
     render() {
+        const { name, profile_pic, status } = this.props.user;
         return (
             <div className='User'>
-                User
+                <img src={profile_pic} alt={name} className='User_pic' />
+                <div className='User_details'>
+                    <p className='User_details_name'>{ name }</p>
+                    <p className='User_details_status'>{ status }</p>
+                </div>
             </div>
         )
     }
